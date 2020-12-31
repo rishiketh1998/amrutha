eeac10a4744df2fb088760afff55852d6288db0d
rishi <prabhak8@uni.coventry.ac.uk>
2020-12-31 05:42:42 +0000

    added register and login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Login } from "./components/login";
+import { Register } from "./components/register";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [ showLogIn, setShowLogIn ] = useState(true)
+  const [ showRegister, setShowRegister ] = useState(false)
   return (
     <div className="App">
-      Hello
+      <Login show={showLogIn} onHide={() => setShowLogIn(false)} showRegister={() => setShowRegister(true)}/>
+      <Register show={showRegister} onHide={() => setShowRegister(false)}/>
     </div>
   );
 }
